c03830c41d54430ac1dda08638f2c5beb3d7c82b
Mohammed Chowdhury <mohammed.2.chowdhury@kcl.ac.uk>
2022-10-15 19:48:05 +0100

    login func

--- microblogs/views.py
+++ microblogs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from microblogs.models import User
 from microblogs.forms import LogInForm, SignUpForm
+from django.contrib.auth import authenticate, login
 
 def home(request):
     return render(request, "home.html")
@@ -19,5 +20,14 @@ def sign_up(request):
     return render(request, "sign_up.html", {"form": form})
 
 def log_in(request):
+    if request.method == "POST":
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("feed")
     form  = LogInForm()
     return render(request, "log_in.html", {"form": form})
